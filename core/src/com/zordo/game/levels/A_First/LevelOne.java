d862a8c69a4e9e604a50681383ec60e791dda5ab
jesus <jesus.s.chavez01@gmail.com>
2024-11-19 00:36:19 -0500

    dynamic level sizes based on config

--- core/src/com/zordo/game/levels/A_First/LevelOne.java
+++ core/src/com/zordo/game/levels/A_First/LevelOne.java
@@ -6,5 +6,6 @@ import com.zordo.game.levels.Level;
 public class LevelOne extends Level {
 	public LevelOne(LegendOfZordo game) {
 		super(game);
+		this.slat.setWidth(game.large.getWidth());
 	}
 }
