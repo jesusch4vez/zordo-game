fb5fe14e3f58c69105020b7815634ba9b0cf1233
jesus <jesus.s.chavez01@gmail.com>
2024-11-19 01:24:43 -0500

    re-organizing structure

---
+++ core/src/com/zordo/model/menu/MenuItem.java
@@ -0,0 +1,9 @@
+package com.zordo.model.menu;
+
+public interface MenuItem {
+
+    String getText();
+    void setText(String name);
+
+    void setEnabled(boolean enabled);
+}
---
+++ core/src/com/zordo/view/levels/B_Second/LevelTwo.java
@@ -0,0 +1,10 @@
+package com.zordo.view.levels.B_Second;
+
+import com.zordo.LegendOfZordo;
+import com.zordo.view.levels.Level;
+
+public class LevelTwo extends Level {
+	public LevelTwo(LegendOfZordo game) {
+		super(game);
+	}
+}
