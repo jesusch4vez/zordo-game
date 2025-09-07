60b26aaee1892d80ca8094f3756a549c20b96675
jesus <jesus.s.chavez01@gmail.com>
2025-09-06 21:20:10 -0400

    vertical camera constraints

--- core/src/main/java/zordo/systems/camera/CameraSystem.java
+++ core/src/main/java/zordo/systems/camera/CameraSystem.java
@@ -7,40 +7,35 @@ import zordo.entities.characters.player.Player;
 public class CameraSystem {
     public static void follow(Player player, OrthographicCamera camera, LegendOfZordo game) {
         if(camera.viewportWidth / camera.position.x < 8 && camera.viewportWidth / camera.position.x > 3 )  {
-            camera.position.set(player.getCharacterComponent().getPosition().x, player.getCharacterComponent().getPosition().y, 0);
+            camera.position.x = player.getCharacterComponent().getPosition().x;
         }
         else if(camera.viewportWidth / camera.position.x >= 8) {
-//        else if(player.getCharacterComponent().getPosition().x < game.activeLevel.rightWall.getX() - 300) {
-            camera.position.y = player.getCharacterComponent().getPosition().y;
             camera.position.x = game.activeLevel.leftWall.getX() + 240;
-//            camera.position.set(player.getCharacterComponent().getPosition().x, player.getCharacterComponent().getPosition().y, 0);
-//        }
             if(player.getCharacterComponent().getPosition().x >= game.activeLevel.leftWall.getX() + 240) {
                 camera.position.x = player.getCharacterComponent().getPosition().x;
             }
         }
         else if(camera.viewportWidth / camera.position.x <= 3) {
-//          else if(player.getCharacterComponent().getPosition().x < game.activeLevel.rightWall.getX() - 300) {
-                camera.position.y = player.getCharacterComponent().getPosition().y;
                 camera.position.x = game.activeLevel.rightWall.getX() - 240;
-//            camera.position.set(player.getCharacterComponent().getPosition().x, player.getCharacterComponent().getPosition().y, 0);
-//        }
             if(player.getCharacterComponent().getPosition().x <= game.activeLevel.rightWall.getX() - 240) {
                 camera.position.x = player.getCharacterComponent().getPosition().x;
             }
         }
-//        else if()  {
-//            camera.position.set(player.getCharacterComponent().getPosition().x, player.getCharacterComponent().getPosition().y, 0);
-//        }
-//        else if(camera.viewportWidth / camera.position.x <= 3) {
-////        else if(player.getCharacterComponent().getPosition().x < game.activeLevel.rightWall.getX() - 300) {
-//            camera.position.y = player.getCharacterComponent().getPosition().y;
-//            camera.position.x = game.activeLevel.rightWall.getX() - 240;
-////            camera.position.set(player.getCharacterComponent().getPosition().x, player.getCharacterComponent().getPosition().y, 0);
-////        }
-////            if(player.getCharacterComponent().getPosition().x <= game.activeLevel.rightWall.getX() - 240) {
-////                camera.position.x = player.getCharacterComponent().getPosition().x;
-////            }
-//        }
+
+        if (camera.viewportHeight / camera.position.y < 8 && camera.viewportHeight / camera.position.y > 4 )  {
+            camera.position.y = player.getCharacterComponent().getPosition().y;
+        }
+        else if(camera.viewportHeight / camera.position.y >= 8) {
+            camera.position.y = game.activeLevel.floor.getY() + 120;
+            if(player.getCharacterComponent().getPosition().y >= game.activeLevel.floor.getY() + 120) {
+                camera.position.y = player.getCharacterComponent().getPosition().y;
+            }
+        }
+        else if(camera.viewportHeight / camera.position.y <= 4) {
+            camera.position.y = game.activeLevel.ceiling.getY() - 120;
+            if(player.getCharacterComponent().getPosition().y <= game.activeLevel.ceiling.getY() - 120) {
+                camera.position.y = player.getCharacterComponent().getPosition().y;
+            }
+        }
     }
 }
